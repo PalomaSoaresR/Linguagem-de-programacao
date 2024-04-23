6703a3812d365a766467f3a454356c5dc7b7fee5
PalomaSoaresR <143560101+PalomaSoaresR@users.noreply.github.com>
2024-04-23 20:46:35 -0300

    Tarefa4

--- Tarefa3/CarroTeste.java
+++ Tarefa3/CarroTeste.java
@@ -2,14 +2,14 @@ public class CarroTeste {
     public static void main(String[] args) {
         Carro c1 = new Carro();
 
-        c1.setMarca("Fiat");
+        c1.setMarca();
         c1.setModelo("Toro");
         c1.setVelocidade(50);
         System.out.println("Marca: " + c1.getMarca() + ", modelo " + c1.getModelo());
 
         c1.acelerar(10);
         c1.freiar(10);
-        c1.bozinar(null);
+        c1.bozinar(true);
 
     }
 
--- Tarefa3/Fracao.java
+++ Tarefa3/Fracao.java
@@ -1,9 +1,7 @@
 public class Fracao {
     private int numerador;
     private int denominador;
-    private int numerador2;
-    private int denominador2;
-
+    
     public int getNumerador() {
         return numerador;
     }
@@ -20,44 +18,28 @@ public class Fracao {
         this.denominador = denominador;
     }
 
-    public int getNumerador2() {
-        return numerador2;
-    }
-
-    public void setNumerador2(int numerador2) {
-        this.numerador2 = numerador2;
-    }
-
-    public int getDenominador2() {
-        return denominador2;
-    }
-
-    public void setDenominador2(int denominador2) {
-        this.denominador2 = denominador2;
-    }
-
     public void somar() {
-        int novodenominador = denominador * denominador2;
-        int novonumerador = (novodenominador / denominador) * numerador + (novodenominador / denominador2) * numerador2;
+        int novodenominador = denominador * denominador;
+        int novonumerador = (novodenominador / denominador) * numerador + (novodenominador / denominador) * numerador;
         System.out.println("A soma foi: " + novonumerador + "/" + novodenominador);
 
     }
 
     public void subtrair() {
-        int novodenominador = denominador * denominador2;
-        int novonumerador = (novodenominador / denominador) * numerador - (novodenominador / denominador2) * numerador2;
+        int novodenominador = denominador * denominador;
+        int novonumerador = (novodenominador / denominador) * numerador - (novodenominador / denominador) * numerador;
         System.out.println("A subtração foi: " + novonumerador + "/" + novodenominador);
     }
 
     public void multiplicar() {
-        int novodenominador = denominador * denominador2;
-        int novonumerador = numerador * numerador2;
+        int novodenominador = denominador * denominador;
+        int novonumerador = numerador * numerador;
         System.out.println("A multiplicação é: " + novonumerador + "/" + novodenominador);
     }
 
     public void dividir() {
-        int novodenominador = denominador * numerador2;
-        int novonumerador = numerador * denominador2;
+        int novodenominador = denominador * numerador;
+        int novonumerador = numerador * denominador;
         System.out.println("A divisão é: " + novonumerador + "/" + novodenominador);
     }
 
